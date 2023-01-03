8cc2b7232764cd22ee5e3b9fdc928dff702a8a72
Hernandexzdev <113794950+Hernandexzdev@users.noreply.github.com>
2023-01-02 20:24:45 -0600

    Update server.js

--- models/server.js
+++ models/server.js
@@ -9,7 +9,7 @@ export class Server {
 
     constructor(){
         this.app  = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuarioPath = '/api/usuarios';
 
         //* Middlewares
